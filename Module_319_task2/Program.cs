46e72d92546e990a3e280d9cef639fd133914ff8
rabbitglauser <samuel.glauser@gmail.com>
2024-09-02 15:34:25 +0200

    Initialize Module_319_task2 project
    Set up a new C# project targeting .NET 8.0, including all necessary project files, cache files, and configurations. Added sample methods for calculating circle perimeter and area in the Program.cs.

---
+++ Module_319_task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//calculating the perimeter 
+void CalculateCirclePerimeter(double radius)
+{
+    radius = 2 * Math.PI * radius;
+    
+    Console.WriteLine($"Radius = {radius}");
+}
+
+CalculateCirclePerimeter(5);
+
+//calculating the circles Area 
+void CalculateCircleArea(double _radius)
+{
+    _radius = Math.PI * double.Round(_radius,2);
+    
+    Console.WriteLine($"_Radius = {_radius}");
+}
+
+CalculateCirclePerimeter(9);
